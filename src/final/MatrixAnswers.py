4b94327226d68bf5d51e6e54baacb6faf2248cfa
nilasini <nilathiru.12@cse.mrt.ac.lk>
2016-09-07 17:55:45 +0530

    testing

--- src/final/MatrixAnswers.py
+++ src/final/MatrixAnswers.py
@@ -11,12 +11,14 @@ row_size = None
 x = ''
 y = ''
 
+#create a logger to show the execution summary
 logging.basicConfig(level=INFO)
 log_name = 'logs'
 logger = logging.getLogger(log_name)
 
 
 class MatrixAnswers:
+    #read the question
     @staticmethod
     def read_question():
         global col_size, row_size, x, y
@@ -24,13 +26,17 @@ class MatrixAnswers:
         isfoundrow = true
         mtrilist = []
         matri_name = []
+        question = ''
         siz = 0
         count = 0
         lhs_dict = {}
-        filein = open('Question_file')
+
+        #open the question file
+        filein = open('2010q.html')
         i = 0
         j = 0
         logger.info('Reading question')
+        #parse the question
         for line in filein:
             soup = BeautifulSoup(line, "html.parser")
             if 'mi' in line and 'mtd' not in line:
@@ -54,6 +60,10 @@ class MatrixAnswers:
             if '/mo' in line and count > 0:
                 if '=' not in soup.text:
                     matri_name.insert(0, soup.text)
+            if '/math' in line and len(matri_name) > 0:
+                for d in range(0, len(matri_name)):
+                    question += matri_name.pop().strip()
+                lhs_dict['ques'] = question
             if '/mtable' in line:
                 if isfoundrow:
                     siz += len(mtrilist)
@@ -74,6 +84,7 @@ class MatrixAnswers:
         logger.info('Finish question reading')
         return lhs_dict
 
+    #read the answers
     @staticmethod
     def mark():
         isfound = true
@@ -84,13 +95,18 @@ class MatrixAnswers:
         siz = 0
         count = 0
         ispassfenced = false
-        iscontain_var = false
+        iscontain_equals = false
         global x, y
+        old_ans_matrix = None
+        ans_matrix = None
+        rightside_constant = None
 
         ques = MatrixAnswers().read_question()
         logger.info('Reading answers')
-        with open('Answer_file') as answer:
+        #open the answer file
+        with open('2010_answer2.html') as answer:
             i = 0
+            #parse the answer file
             for line in answer:
                 ans_soup = BeautifulSoup(line, "html.parser")
                 if 'mi' in line and 'mtd' not in line and not ispassfenced:
@@ -102,29 +118,43 @@ class MatrixAnswers:
                     ispassfenced = true
                 if '/mfenced' in line:
                     ispassfenced = false
-                if 'mn' in line:
+                    rightside_constant = None
+                if 'mn' in line and 'mtd' in line:
+                    if rightside_constant:
+                        s_text = int(rightside_constant) * int(ans_soup.text)
+                        mtrilist.insert(i, str(s_text))
+                        # if 'x' in str(s_text) or 'y' in str(s_text):
+                        #     iscontain_var = true
+                    else:
                         mtrilist.insert(i, ans_soup.text)
-                        if 'x' in ans_soup.text or 'y' in ans_soup.text:
-                            iscontain_var = true
+                        # if 'x' in ans_soup.text or 'y' in ans_soup.text:
+                        #     iscontain_var = true
+                if 'mn' in line and 'mtd' not in line:
+                    rightside_constant = ans_soup.text
                 if '/mtr' in line and isfound:
                     col_size = len(mtrilist)
                     isfound = false
                 if '/mo' in line and count > 0:
                     if '=' not in ans_soup.text:
                         matri_name.insert(0, ans_soup.text)
+                if '/mo' in line and count == 0:
+                    if ans_soup.text == '=':
+                        iscontain_equals = ans_soup.text
                 if '/mtable' in line:
                     if isfoundrow:
                         siz += len(mtrilist)
                         row_size = int(siz / col_size)
                         isfoundrow = false
+                if 'mspace' in line:
                     list = [mtrilist[x:x + col_size] for x in range(0, len(mtrilist), col_size)]
-                # check whether the lefthand elements contains numbers with variables. Eg:2A
+
+                iscontainplus = false
+                iscontainminus = false
+                #if student write lefthand side and right hand side
                 if matri_name and list:
                     length = len(matri_name)
-                    iscontainplus = false
-                    iscontainminus = false
                     matrix_leftside = ''
-
+                    #more than one matrix involved
                     if length > 2:
                         for d in range(0, length):
                             matrix_leftside += matri_name.pop().strip()
@@ -149,32 +179,108 @@ class MatrixAnswers:
                             ans_matrix = matrix_1 + matrix_2
                         elif iscontainminus:
                             ans_matrix = matrix_1 - matrix_2
-                        subs_matrix = ans_matrix - Matrix(list)
+                        if len(list) == row_size:
+                            subs_matrix = ans_matrix - Matrix(list)
+                        else:
+                            stu_ans_matr1 = list[0:row_size]
+                            stu_ans_matr2 = list[row_size:len(list)]
+                            if iscontainplus:
+                                stu_ans = Matrix(stu_ans_matr1) + Matrix(stu_ans_matr2)
+                            elif iscontainminus:
+                                stu_ans = Matrix(stu_ans_matr1) - Matrix(stu_ans_matr2)
+                            subs_matrix = ans_matrix - stu_ans
                         if subs_matrix == zeros(row_size, col_size):
                             logger.info('your step is correct')
                         list.clear()
                         mtrilist.clear()
                         isfound = true
                         isfoundrow = true
+                        old_ans_matrix = ans_matrix
                         matri_name.clear()
                         siz = 0
                         count = 0
-                        iscontain_var = false
+                    #only one matrix involved
                     else:
                         matrix_leftside = matri_name.pop().strip()
                         if re.search(r'\d', matrix_leftside):
-                            simpleleftmatrix = int(matrix_leftside[0:1]) * ques[matrix_leftside[1:2]]
-                            subs_matrix = simpleleftmatrix - Matrix(list)
+                            ans_matrix = int(matrix_leftside[0:1]) * ques[matrix_leftside[1:2]]
+                            subs_matrix = ans_matrix - Matrix(list)
                             if subs_matrix == zeros(row_size, col_size):
                                 logger.info('your step is correct')
                             list.clear()
                             mtrilist.clear()
                             isfound = true
                             isfoundrow = true
+                            old_ans_matrix = ans_matrix
                             matri_name.clear()
                             siz = 0
                             count = 0
-                            iscontain_var = false
+                #if student didn't write any lefthand side rather only right hand side answers were there
+                elif list:
+                    if iscontain_equals:
+                        if len(list) == row_size:
+                            subs_matrix = old_ans_matrix - Matrix(list)
+                        else:
+                            stu_ans_matr1 = list[0:row_size]
+                            stu_ans_matr2 = list[row_size:len(list)]
+                            if iscontainplus:
+                                stu_ans = Matrix(stu_ans_matr1) + Matrix(stu_ans_matr2)
+                            elif iscontainminus:
+                                stu_ans = Matrix(stu_ans_matr1) - Matrix(stu_ans_matr2)
+                            subs_matrix = old_ans_matrix - stu_ans
+                        if subs_matrix == zeros(row_size, col_size):
+                            logger.info('your step is correct')
+                        list.clear()
+                        mtrilist.clear()
+                        isfound = true
+                        isfoundrow = true
+                        old_ans_matrix = ans_matrix
+                        matri_name.clear()
+                        siz = 0
+                        count = 0
+                    else:
+                        matrix_leftside = ques['ques']
+                        if "-" in matrix_leftside:
+                            iscontainminus = true
+                            # split the left hand side using -
+                            twooperandsplitarray = matrix_leftside.split('-')
+                        elif "+" in matrix_leftside:
+                            iscontainplus = true
+                            # split the left hand side using +
+                            twooperandsplitarray = matrix_leftside.split('+')
+                        if len(twooperandsplitarray[0]) > 1:
+                            matrix_1 = int(twooperandsplitarray[0][0:1]) * ques[twooperandsplitarray[0][1:2]]
+                        elif len(twooperandsplitarray[0]) <= 1:
+                            matrix_1 = ques[twooperandsplitarray[0]]
+                        if len(twooperandsplitarray[1]) > 1:
+                            matrix_2 = int(twooperandsplitarray[1][0:1]) * ques[twooperandsplitarray[1][1:2]]
+                        elif len(twooperandsplitarray[1]) <= 1:
+                            matrix_2 = ques[twooperandsplitarray[1]]
+
+                        if iscontainplus:
+                            ans_matrix = matrix_1 + matrix_2
+                        elif iscontainminus:
+                            ans_matrix = matrix_1 - matrix_2
+                        if len(list) == row_size:
+                            subs_matrix = ans_matrix - Matrix(list)
+                        else:
+                            stu_ans_matr1 = list[0:row_size]
+                            stu_ans_matr2 = list[row_size:len(list)]
+                            if iscontainplus:
+                                stu_ans = Matrix(stu_ans_matr1) + Matrix(stu_ans_matr2)
+                            elif iscontainminus:
+                                stu_ans = Matrix(stu_ans_matr1) - Matrix(stu_ans_matr2)
+                            subs_matrix = ans_matrix - stu_ans
+                        if subs_matrix == zeros(row_size, col_size):
+                            logger.info('your step is correct')
+                        list.clear()
+                        mtrilist.clear()
+                        isfound = true
+                        isfoundrow = true
+                        old_ans_matrix = ans_matrix
+                        matri_name.clear()
+                        siz = 0
+                        count = 0
                 i += 1
         logger.info('finish answer reading')
         answer.close()
